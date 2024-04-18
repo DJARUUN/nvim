6ed3fb9d4ecb1ade6652426b20bbd5e2ef85eaa9
djaruun <caargajac@gmail.com>
2024-04-18 21:03:24 +0200

    things

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,33 +2,33 @@ local M = {}
 
 function M.run()
 	-- general
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "colorscheme poimandres",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight NonText guifg=#1b1e28",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight NormalFloat guibg=#1b1e28",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight FloatBorder guifg=#303340",
 	})
 
 	-- whichkey
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight WhichKeyFloat guibg=#1b1e28",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight WhichKeySeparator guifg=#a6accd",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight WhichKeyDesc guifg=#add7ff",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight WhichKeyGroup guifg=#91b4d5",
 	})
-	vim.api.nvim_create_autocmd({ "User" }, {
+	vim.api.nvim_create_autocmd({ "VimEnter" }, {
 		command = "highlight WhichKeyBorder guifg=#303340",
 	})
 end
