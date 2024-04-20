d097174ebc13fbf540ad3fcd934162956878387d
djaruun <caargajac@gmail.com>
2024-04-20 22:57:34 +0200

    d

--- lua/plugins/oil_nvim.lua
+++ lua/plugins/oil_nvim.lua
@@ -7,7 +7,6 @@ return {
 				default_file_explorer = true,
 				columns = {
 					"icon",
-					"permissions",
 					"size",
 					"mtime",
 				},
@@ -26,7 +25,7 @@ return {
 					concealcursor = "nvic",
 				},
 				delete_to_trash = true,
-				skip_confirm_for_simple_edits = false,
+				skip_confirm_for_simple_edits = true,
 				prompt_save_on_select_new_entry = true,
 				cleanup_delay_ms = 2000,
 				lsp_file_methods = {
